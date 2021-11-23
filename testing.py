2e3f3445f5adf5beb9caab7360d386e8bf97cddf
Andrew Paulikas <PaulikasA@gmail.com>
2021-11-23 15:01:11 -0500

    Formatting

--- testing.py
+++ testing.py
@@ -1,13 +1,15 @@
 from Linked_List import Linked_List
 
 print("_____Linked List_____")
-print("-----Insert Beginning-----")
+
+print("-----insert_beginning()-----")
 ll = Linked_List()
 ll.insert_beginning("hello")
 ll.insert_beginning("me")
 ll.insert_beginning("its")
 print(ll.stringify_list())
-print("-----Remove Node-----")
+
+print("-----remove_node()-----")
 # tests if value is not in list
 print(ll.remove_node("Im not here"))
 #removes node from middle of list
@@ -19,7 +21,8 @@ ll.remove_node("hello")
 #trys to remove a node from an empty list
 ll.remove_node("Empty")
 print(ll.stringify_list())
-print("------Get Head Node-----")
+
+print("------get_head_node()-----")
 print(ll.get_head_node())
 ll.insert_beginning(3)
 print(ll.get_head_node())
