19f0bd2a6825776c1efa720e25ff049905d5d078
Andrew Paulikas <PaulikasA@gmail.com>
2021-11-23 14:12:24 -0500

    added tests to get_head_node()

--- testing.py
+++ testing.py
@@ -9,9 +9,9 @@ ll.insert_beginning("its")
 print(ll.stringify_list())
 print("-----Remove Node-----")
 # tests if value is not in list
-ll.remove_node("Im not here")
+print(ll.remove_node("Im not here"))
 #removes node from middle of list
-ll.remove_node("me")
+print(ll.remove_node("me"))
 #removes node from front of list
 ll.remove_node("its")
 #removed only node in list
@@ -19,3 +19,11 @@ ll.remove_node("hello")
 #trys to remove a node from an empty list
 ll.remove_node("Empty")
 print(ll.stringify_list())
+print("------Get Head Node-----")
+print(ll.get_head_node())
+ll.insert_beginning(3)
+print(ll.get_head_node())
+ll.insert_beginning(2)
+print(ll.get_head_node())
+ll.insert_beginning(1)
+print(ll.get_head_node())
