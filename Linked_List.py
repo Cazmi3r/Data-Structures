bd5cb3a6b150b1018e293370699dce640212761b
Andrew Paulikas <PaulikasA@gmail.com>
2021-11-23 09:38:36 -0500

    removed testing code from Linked List, created Testing Script

--- Linked_List.py
+++ Linked_List.py
@@ -61,16 +61,3 @@ def nth_last_node(linked_list, n):
       else:
         nth_last_pointer = nth_last_pointer.get_next_node()
   return nth_last_pointer
-
-
-def generate_test_linked_list():
-  linked_list = Linked_List()
-  for i in range(50):
-    linked_list.insert_beginning(i)
-  return linked_list
-
-# Use this to test your code:
-test_list = generate_test_linked_list()
-print(test_list.stringify_list())
-nth_last = nth_last_node(test_list, 2)
-print(nth_last.value)
---
+++ testing.py
@@ -0,0 +1,8 @@
+from Linked_List import Linked_List
+
+
+ll = Linked_List()
+ll.insert_beginning("hello")
+ll.insert_beginning("me")
+ll.insert_beginning("its")
+print(ll.stringify_list())
