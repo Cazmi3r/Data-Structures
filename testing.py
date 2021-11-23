42fff31bfda2667892db9cf31fabb9ac96d05924
Andrew Paulikas <PaulikasA@gmail.com>
2021-11-23 15:11:10 -0500

    updated get_head_node() tests and remove_node() tests

--- testing.py
+++ testing.py
@@ -15,18 +15,18 @@ print(ll.remove_node("Im not here"))
 #removes node from middle of list
 print(ll.remove_node("me"))
 #removes node from front of list
-ll.remove_node("its")
+print(ll.remove_node("its"))
 #removed only node in list
-ll.remove_node("hello")
+print(ll.remove_node("hello"))
 #trys to remove a node from an empty list
-ll.remove_node("Empty")
+print(ll.remove_node("Empty"))
 print(ll.stringify_list())
 
 print("------get_head_node()-----")
 print(ll.get_head_node())
 ll.insert_beginning(3)
-print(ll.get_head_node())
+print(ll.get_head_node().get_value())
 ll.insert_beginning(2)
-print(ll.get_head_node())
+print(ll.get_head_node().get_value())
 ll.insert_beginning(1)
-print(ll.get_head_node())
+print(ll.get_head_node().get_value())
