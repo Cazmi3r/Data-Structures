a46bb323519fb8dc0b131629a32d5d621a6fc212
Raxath <36684140+Raxath@users.noreply.github.com>
2021-12-02 21:19:45 -0500

    added tests

--- Doubly_Linked_List_Test.py
+++ Doubly_Linked_List_Test.py
@@ -7,7 +7,28 @@ print("_____Doubly_Linked_List_____")
 
 print("-----add_to_head-----")
 # add to an empty list
-ll.add_to_head(2)
+ll.add_to_head(3)
 # add to list with one element
+ll.add_to_head(2)
+# add to list with more than one element
 ll.add_to_head(1)
+print(ll.stringify_list())
+
+print("-----add_to_tail-----")
+ll = Doubly_Linked_List()
+# add to an empty list
+ll.add_to_tail(1)
+# add to list with one element
+ll.add_to_tail(2)
+# add to list with more than one element
+ll.add_to_tail(3)
+print(ll.stringify_list())
+
+print("-----remove_head-----")
+# remove from list with more than 2 elements
+ll.remove_head()
+# remove from list with 2 elements
+ll.remove_head()
+ll.remove_head()
+ll.remove_head()
 print(ll.stringify_list())
